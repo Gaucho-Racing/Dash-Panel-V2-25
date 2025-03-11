9e67b0a4d207d874214f3faec5f70c6ff2331472
Bailey Say <baileyasay@gmail.com>
2025-03-10 23:33:01 -0700

    starting CAN implementation

---
+++ Application/Src/CANdler.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
+    UNUSED(timestamp);
+
+    switch (msgID) {
+
+        case 
+
+    }
+
+}

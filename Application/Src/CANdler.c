158a24d2cdf43a0cd87d06b4e818901baf5aa74a
Bailey Say <baileyasay@gmail.com>
2025-03-10 23:33:03 -0700

    starting CAN implementation

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -1,12 +1,54 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "CANdler.h"
+#include "msgIDs.h"
+#include "grIDs.h"
+
+volatile uint8_t numberOfBadMessages = 0;
+
 void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
     UNUSED(timestamp);
 
     switch (msgID) {
 
-        case 
+        case MSG_DEBUG_2_0:
+            if (length > 8) {
+                numberOfBadMessages++;
+                return;
+            } else {
+                numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
+            }
+
+            char* debug2String = (char*)data;
+            UNUSED(debug2String);
+
+            break;
+        
+        case MSG_PING:
+            if (length != 4) {
+                numberOfBadMessages++;
+                return;
+            } else {
+                numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
+            }
+
+            respondToPing(srcID, *(uint32_t*)data);
+
+            break;
+
+        case MSG_DASH_CONFIG:
+
+            break;
+
+        case MSG_DASH_WARNING_FLAGS:
+
+            break;
+
+        case MSG_STEERING_STATUS:
+
+            break;
+        
 
     }
 
---
+++ Application/Src/pinging.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+#include "CANdler.h"
+#include "pinging.h"
+#include "msgIDs.h"
+#include "grIDs.h"
+
+void respondToPing(uint8_t destID, uint32_t timestamp) {
+
+}

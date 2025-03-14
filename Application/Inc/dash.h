9a2d429d3e9da7488e3477f1fdaf98c77f80a1d4
Daniel Hansen <dchansen06@gmail.com>
2025-03-13 20:03:43 -0700

    Adding comments

--- Application/Inc/dash.h
+++ Application/Inc/dash.h
@@ -3,6 +3,20 @@
 #ifndef DASH_H
 #define DASH_H
 
+typedef enum {
+    GLV_ON = 1,
+    PRECHARGE_ENGAGED = 2,
+    PRECHARGING = 3,
+    PRECHARGE_COMPLETE = 4,
+    DRIVE_STANDBY = 5,
+    DRIVE_ACTIVE_IDLE = 6,
+    DRIVE_ACTIVE_POWER = 7,
+    DRIVE_ACTIVE_REGEN = 8,
+    TS_DISCHARGE_OFF = 9,
+    REFLASH_TUNE = 10,
+    ERRORSTATE = 11
+} ECUState;
+
 typedef struct {
     struct {
         uint8_t ledBits;
@@ -11,12 +25,12 @@ typedef struct {
     } DashStatusMsg;
 
     uint8_t bseAppsViolation;
-    uint16_t vehicleSpeed;
-    uint8_t maxCellTemp;
-    uint16_t dtiAcCurrent;
-    uint8_t accumulatorStateOfCharge;
-    uint8_t motorTemperature[4];
-    uint8_t ecuState;
+    uint16_t vehicleSpeed; // From ECU Status
+    uint8_t maxCellTemp; // Max cell temp from entire car
+    uint16_t dtiAcCurrent; // Current of back motor
+    uint8_t accumulatorStateOfCharge; // SOC aka battery life
+    uint8_t motorTemperature[4]; // Temperatures for all 4(?) motors
+    ECUState ecuState; // Current state of the car
 
 } DashInfo;
 

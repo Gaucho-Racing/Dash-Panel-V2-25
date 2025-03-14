67087c1d38c15aa42f04a9170b35c878bc85f157
Daniel Hansen <dchansen06@gmail.com>
2025-03-13 20:24:24 -0700

    Custom IDs made

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -48,6 +48,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
             }
 
             DashStatusMsg* dashStatusMsg = (DashStatusMsg*) data;
+            globalStatus.
 
             break;
 

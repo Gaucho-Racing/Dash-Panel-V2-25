8a093f5e05dccbf9a2b5eecb2169f19a8eba7a4c
Daniel Hansen <dchansen06@gmail.com>
2025-04-01 02:03:53 -0700

    Remove some small warnings

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -25,7 +25,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
                 numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
             }
 
-            memcpy(globalStatus.debugMessage, data, 8);
+            memcpy((char*)globalStatus.debugMessage, data, 8);
 
             break;
         

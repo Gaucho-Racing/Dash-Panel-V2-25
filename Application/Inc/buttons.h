473095e17f5f3655148dff5112fd6dd13dbae00c
Daniel Hansen <dchansen06@gmail.com>
2025-04-10 22:25:07 -0700

    Fixed dumb button thingie

--- Application/Inc/buttons.h
+++ Application/Inc/buttons.h
@@ -3,12 +3,5 @@
 #ifndef BUTTONS_H
     #define BUTTONS_H
 
-    typedef struct {
-        bool rtd;
-        bool tsActive;
-    } ButtonState;
-
-    extern volatile ButtonState globalButtonState;
-
     void pollButtonState(void);
 #endif

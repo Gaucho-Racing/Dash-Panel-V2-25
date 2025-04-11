4cdb4c7898442493e938866346b30840638c7544
Daniel Hansen <dchansen06@gmail.com>
2025-04-10 22:14:21 -0700

    Implemented stuff and did not break building

--- Application/Src/dash.c
+++ Application/Src/dash.c
@@ -5,7 +5,7 @@ volatile DashInfo globalStatus = {0};
 int32_t prevRefresh = BAD_TIME_Negative1;
 int32_t tempsRefreshRateMillis = 50;
 
-void newDataPleaseRefresh(void)
+void recievedNewInformationPleaseRefresh(void)
 {
     // TODO: Implement functionality to call for a refresh of the screen based off of the just-updated DashInfo from CANFD
 }

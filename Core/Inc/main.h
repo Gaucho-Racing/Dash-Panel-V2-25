4781cf29e1b27ad39c351738bf9f30e632a583ee
Daniel Hansen <dchansen06@gmail.com>
2025-04-17 21:26:03 -0700

    Missing color fix

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -99,11 +99,11 @@ void Error_Handler(void);
 
 /* USER CODE BEGIN Private defines */
 // See Brand Guidelines
-#define GR_COLOR_WHITE 0xFFFFFF
 #define GR_COLOR_PINK 0xEF0DA1
 #define GR_COLOR_PURPLE 0x7920FF
 #define GR_COLOR_NAVY 0x195297
 #define GR_COLOR_GRAY 0x9AA3B0
+#define GR_COLOR_WHITE 0xFFFFFF
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus

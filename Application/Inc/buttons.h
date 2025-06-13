230e22dce0c4ad87292907cfa4f37c9dbf77e530
Daniel Hansen <dchansen06@gmail.com>
2025-06-12 21:42:56 -0700

    Reorder sry

--- Application/Inc/buttons.h
+++ Application/Inc/buttons.h
@@ -14,17 +14,17 @@
     } ButtonNames;
     // Must be 1 or 2 for LED1 or LED2 respectively
 
-    typedef struct {
-        Color TS_Active;
-        Color RTD;
-    } NeoPixelData;
-
     // Format is GRB
     typedef enum {
         COLOR_GREEN = (uint32_t)0xFF0000,
         COLOR_RED = (uint32_t)0x00FF00,
         COLOR_BLUE = (uint32_t)0x0000FF,
     } Color;
+
+    typedef struct {
+        Color TS_Active;
+        Color RTD;
+    } NeoPixelData;
 
     void pollButtonState(void* args);
     void updateButtonColors(void* args);
